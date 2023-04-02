786254fb7dddc6d9ec21c289dfcf7885fcc051d2
qyuanwen <122020301+qyuanwen@users.noreply.github.com>
2023-04-02 23:44:56 +0800

    Update 01_SMRT_Delays_Tableau.py

--- pages/01_SMRT_Delays_Tableau.py
+++ pages/01_SMRT_Delays_Tableau.py
@@ -2,7 +2,7 @@ import streamlit as st
 from streamlit.components.v1 import components
 
 # Embedding Tableau dashboard in Streamlit app
-tableau_url = "https://your-tableau-dashboard-url"
+tableau_url = "https://public.tableau.com/views/SMRT_Delays/Dashboard?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link"
 tableau_width = "100%"
 tableau_height = "900" # set the height to a fixed value or adjust as needed
 
